9c1f163b94d96da1eca5ca017230037e964f5cb1
demade74 <demade74@gmail.com>
2021-06-17 17:06:32 +0300

    Homework done

---
+++ lesson6/avito/spiders/selectors.py
@@ -0,0 +1,7 @@
+APT_DATA = {
+    "title": "//h1[@class='title-info-title']/span/text()",
+    "price": "//span[@itemprop='price']/@content",
+    "address": "//span[@class='item-address__string']/text()",
+    "params": "//ul[@class='item-params-list']/li",
+    "author": "//div[@class='seller-info-name']/a/@href"
+}
